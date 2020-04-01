542362fa3fc8346b557c1bce5361c76f634ed662
Suresh <nsk126@gmail.com>
2020-04-02 04:30:55 +0530

    more

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const Discord = require('discord.js');
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 const bot = new Discord.Client();
-const token = "";
+const token = "Njk0MDczNDU1MTIwNzQ0NDU4.XoN3Eg.wNr-zNNhl5NU0KXP9d6U3YfGcFo";
 
 const prefix = '??';
+const path = 'images/recieve.png';
 
 bot.on('ready',() => {
     console.log(`Logged in as ${bot.user.tag}!`);
@@ -13,7 +15,7 @@ bot.on('ready',() => {
 bot.on('message', message =>{
     let arg = message.content.substring(prefix.length).split(" ");
     switch(arg[0]){
-        case 'hello':
+        case 'hi':
             message.channel.send("Hello !");
             break;
         case 'help':
@@ -21,10 +23,14 @@ bot.on('message', message =>{
                 .setColor('#FF0000')
                 .setTitle('All Commands')
                 .addFields(
-                    {name: '??hello', value: 'Hello !'},
+                    {name: '??hi', value: 'Hello !'},
                     {name: '??help', value: 'this menu !'},
                     {name: '??ava', value: 'shows your avatar'},
-                    {name: '??boat', value: 'shows an image'},
+                    // {name: '??boat', value: 'BD - Best of All times leaderboard'},
+                    {name: '??BD', value: 'Battle-Dawn'},
+                    {name: '??dist', value: '??dist Cord1 Cord2'},
+                    {name: '??mil', value: 'Military scan results: N number of scans '},
+                    {name: '??adv', value: 'Advance scan results: N number of scans '},
                 );
             message.channel.send(embed);
             break;
@@ -35,20 +41,124 @@ bot.on('message', message =>{
             const attachment = new Discord.MessageAttachment('images/anime-girl.jpg');
             message.channel.send(attachment);
             break;
-        case 'boat':
-            (async () => {
-                const browser = await puppeteer.launch();
-                const page = await browser.newPage();
-                await page.goto('http://www.battledawn.com/?p=high_score');
-                await page.screenshot({path: 'images/recieve.png'});
-                await browser.close();
-              })();
-            const recimg = new Discord.MessageAttachment('images/recieve.png');  
-            message.channel.send(recimg);
+        case 'BD':
+            const BD_link = 'http://battledawn.com/client/game.php?portal=earthMarsFantasy';
+            message.channel.send(BD_link);
             break;
+        case 'dist':
+            // N:2697 E:9598 <- example arg
+            // N:6940 E:37334 N:5972 E:36621
+
+            var cord1N = arg[1].substring(2,arg[1].length);
+            var cord1E = arg[2].substring(2,arg[2].length);
+            var cord2N = arg[3].substring(2,arg[3].length);
+            var cord2E = arg[4].substring(2,arg[4].length);
+            
+            cord1N = parseInt(cord1N);
+            cord1E = parseInt(cord1E);
+            cord2N = parseInt(cord2N);
+            cord2E = parseInt(cord2E);
+            
+            var x = cord1N - cord2N;
+            var y = cord1E - cord2E;
+            var d = x**2 + y**2;
+            d = Math.sqrt(d);
+            d = Math.round(d);
+            /*
+            Distance Log
+                Hostile ETA
+                400KM - 2 , not applied
+                600KM - 3
+                800KM - 4
+                1000KM - 5
+                1200KM - 6 ... so on
+            */
+
+            var hETA = Math.ceil(d/200);
+            console.log(hETA);
+            hETA = Math.max(hETA,3);
+            console.log(hETA);
+            var fETA = Math.ceil(hETA/2);
+            fETA = Math.max(fETA,2);
+
+            message.reply("\nDistance(Km) = " + d + "\n" +"Hostile ETA = " + hETA + "\nFriendly ETA = " + fETA);
+            break;
+
+        case 'mil':
+            // console.log(arg.length);
+            // Mil scan accuracy +-50% 
+            // Wiki says 66% to 200% Standard Deviation
+            var scan = [];
+            var Lscan = [];
+            var Uscan = [];
+            for(i = 0;i < arg.length-1; i++){
+                scan[i] = arg[i+1];
+            }
+            // console.log(scan);
+            for(i = 0;i < arg.length-1; i++){
+                Lscan[i] = scan[i] * 0.66;
+                Uscan[i] = scan[i] * 2.00;
+            }
+            // console.log(Lscan);
+            // console.log(Uscan);
+            var Lbound = Math.max(...Lscan).toFixed(2);;
+            var Ubound = Math.min(...Uscan).toFixed(2);;
+            message.reply("\nMax units = " + Ubound + "\nMin units = " + Lbound);
+            break;
+        case 'adv':
+            // adv scan accuracy +-25% 
+            // Wiki says 80% to 130% Standard Deviation
+            var scan = [];
+            var Lscan = [];
+            var Uscan = [];
+            for(i = 0;i < arg.length-1; i++){
+                scan[i] = arg[i+1];
+            }
+            // console.log(scan);
+            for(i = 0;i < arg.length-1; i++){
+                Lscan[i] = scan[i] * 0.80;
+                Uscan[i] = scan[i] * 1.33;
+            }
+            // console.log(Lscan);
+            // console.log(Uscan);
+            var Lbound = Math.max(...Lscan).toFixed(2);
+            var Ubound = Math.min(...Uscan).toFixed(2);
+            message.reply("\nMax units = " + Ubound + "\nMin units = " + Lbound);
+            break;
+
+        // case 'boat':
+        //     // let gotoURL = arg[1];
+        //     (async () => {
+        //         const browser = await puppeteer.launch();
+        //         const page = await browser.newPage();
+        //         // await page.goto(gotoURL);
+        //         await page.goto('http://www.battledawn.com/?p=high_score');
+        //         await page.screenshot({path: 'images/recieve.png'});
+        //         await browser.close();
+        //       })();
+            
+        //     const recimg = new Discord.MessageAttachment('images/recieve.png');  
+        //     message.channel.send(recimg);
+        //     break;
 
     }
         
 })
 
 bot.login(token);
+
+
+
+
+
+
+
+
+/**puppeteer.launch().then(browser => {
+                browser.newPage()
+                  .then(page => {
+                    page.goto('http://www.battledawn.com/?p=high_score')
+                      .then(resp => page.screenshot({path: 'images/recieve.png'}))
+                      .then(buffer => browser.close());
+                  });
+              }); */
